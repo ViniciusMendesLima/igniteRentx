73ac497ea74b969df27b2d2f4595cfa7340d2215
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2023-01-28 09:51:30 -0300

    Finalizando CreateCarSpecificationUseCase

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
@@ -10,6 +11,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsCarController = new ListAvailableCarsController();
+const crateCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   "/",
@@ -19,5 +21,12 @@ carsRoutes.post(
 );
 
 carsRoutes.get("/available", listAvailableCarsCarController.handle);
+
+carsRoutes.post(
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  crateCarSpecificationController.handle
+);
 
 export { carsRoutes };
