c025cbebd51c8540ba4d4a31aa4fcf9272f946fd
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2023-02-11 09:47:05 -0300

    refatorando autenticação do usuario

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "3a110dfcdc92a47c6b78a7ec3bd78bd1",
+  expires_in_token: "15m",
+  secret_refresh_token: "edea0ea543bc32f0c8a21bd08272b480",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
