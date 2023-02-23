8a56fc148b7e060d51b7f8b688953c0552b45def
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2023-02-23 00:20:07 -0300

    creando URL de acesso do avatar

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,13 +6,13 @@ import { categoriesRoutes } from "./categories.routes";
 import { passwordRoutes } from "./password.routes";
 import { rentalRoutes } from "./rental.routes";
 import { specificationsRoutes } from "./specifications.routes";
-import { userRoutes } from "./users.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
-router.use("/users", userRoutes);
+router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
 router.use("/rentals", rentalRoutes);
 router.use("/password", passwordRoutes);
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,23 +3,27 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
-const userRoutes = Router();
+const usersRoutes = Router();
 
 const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
-userRoutes.post("/", createUserController.handle);
+usersRoutes.post("/", createUserController.handle);
 
-userRoutes.patch(
+usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
 
-export { userRoutes };
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
+
+export { usersRoutes };
